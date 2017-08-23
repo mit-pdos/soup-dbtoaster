e7399ae9d095e357ad7778057ff8fa17f3ada3f5
Malte Schwarzkopf <malte@csail.mit.edu>
2017-08-23 19:01:35 -0400

    Allow running script from anywhere

--- vote/generate_test_data.sh
+++ vote/generate_test_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "generating 100k articles..."
-for i in $(seq 0 99999); do echo "$i,\"Article #$i\"" >> art.csv; done
+for i in $(seq 0 99999); do echo "$i,\"Article #$i\"" >> ${dir}/art.csv; done
 
 echo "generating 50M votes, this can take a moment..."
-for j in $(seq 500); do for i in $(seq 0 99999); do echo "0,$i" >> vt.csv; done; done
+for j in $(seq 500); do for i in $(seq 0 99999); do echo "0,$i" >> ${dir}/vt.csv; done; done
