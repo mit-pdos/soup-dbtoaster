3ad4a99b43441efa346cac951088282157530787
Jon Gjengset <jon@thesquareplanet.com>
2017-08-28 13:01:02 -0400

    Report generation progress

--- vote/generate_test_data.sh
+++ vote/generate_test_data.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "generating 100k articles..."
+echo "==> generating 100k articles..."
 for i in $(seq 0 99999); do echo "$i,\"Article #$i\"" >> ${dir}/art.csv; done
 
-echo "generating 50M votes, this can take a moment..."
-for j in $(seq 500); do for i in $(seq 0 99999); do echo "0,$i" >> ${dir}/vt.csv; done; done
+echo "==> generating 50M votes, this can take a moment..."
+for j in $(seq 500); do
+	echo "  -> $j/500"
+	for i in $(seq 0 99999); do echo "0,$i" >> ${dir}/vt.csv;
+	done;
+done
