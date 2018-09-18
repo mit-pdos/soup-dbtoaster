0b1dcebd287839c73758bb94a4062cf547c28703
Jon Gjengset <jon@thesquareplanet.com>
2018-09-18 13:59:31 -0400

    Generate much faster

--- vote/generate_test_data.sh
+++ vote/generate_test_data.sh
@@ -7,10 +7,14 @@ for i in $(seq 0 499999); do echo "$i,\"Article #$i\"" >> "${dir}/art.csv"; done
 
 echo "==> generating 50M votes, this can take a moment..."
 rm "${dir}/vt.csv" "${dir}/vt-ord.csv"
+for i in $(seq 0 499999); do echo "0,$i" >> "${dir}/vt-tmp.csv";
+done;
+
 for j in $(seq 100); do
 	echo "  -> $j/100"
-	for i in $(seq 0 499999); do echo "0,$i" >> "${dir}/vt-ord.csv";
-	done;
+	cat "${dir}/vt-tmp.csv" >> "${dir}/vt-ord.csv";
 done
+rm "${dir}/vt-tmp.csv"
+
 shuf -o "${dir}/vt.csv" "${dir}/vt-ord.csv"
 rm "${dir}/vt-ord.csv"
