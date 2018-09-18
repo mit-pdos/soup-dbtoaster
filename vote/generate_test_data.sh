5ec1cda7141ddcbe774186a633c30064f71b2f16
Jon Gjengset <jon@thesquareplanet.com>
2018-09-18 13:57:27 -0400

    Use 500k articles

--- vote/generate_test_data.sh
+++ vote/generate_test_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "==> generating 100k articles..."
+echo "==> generating 500k articles..."
 rm "${dir}/art.csv"
-for i in $(seq 0 99999); do echo "$i,\"Article #$i\"" >> "${dir}/art.csv"; done
+for i in $(seq 0 499999); do echo "$i,\"Article #$i\"" >> "${dir}/art.csv"; done
 
 echo "==> generating 50M votes, this can take a moment..."
 rm "${dir}/vt.csv" "${dir}/vt-ord.csv"
-for j in $(seq 500); do
-	echo "  -> $j/500"
-	for i in $(seq 0 99999); do echo "0,$i" >> "${dir}/vt-ord.csv";
+for j in $(seq 100); do
+	echo "  -> $j/100"
+	for i in $(seq 0 499999); do echo "0,$i" >> "${dir}/vt-ord.csv";
 	done;
 done
 shuf -o "${dir}/vt.csv" "${dir}/vt-ord.csv"
