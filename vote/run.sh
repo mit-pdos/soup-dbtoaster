6871be053d6cc711464a4edaec6e57e6dadac298
Jon Gjengset <jon@thesquareplanet.com>
2018-09-18 18:57:46 -0400

    Let dbt do tuple-at-a-time

--- vote/run.sh
+++ vote/run.sh
@@ -3,6 +3,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(dir)/../dbtoaster/lib/dbt_c++/libdbtoaster.a
 
-${dir}/../dbtoaster/bin/dbtoaster -O3 --batch vote.sql -c vote
+${dir}/../dbtoaster/bin/dbtoaster -O3 vote.sql -c vote
 
-time ./vote --batch-size 2500 -n
+time -v ./vote -n
